2d1205e193dec39579835bec71b63439bf5f19da
DphnZwp <zwuupdaphne@gmail.com>
2022-10-10 18:41:01 +0200

    Create story section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,17 +5,29 @@
   console.log(stories);
 </script>
 
+  <header class="hero">
+    <h1 class="hero__heading">Read a story</h1>
+  </header>
+
   <section class="create-story">
     <img class="create-story__clock" src="/clock.png" alt="">
+    <h2 class="create-story__heading">Click on the book to create a story</h2>
+    <svg class="create-story__icon" width="62" height="65" viewBox="0 0 62 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M32 7L32 57.2765" stroke="white" stroke-width="14" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M7 32.1382L31 57.2765L55 32.1382" stroke="white" stroke-width="14" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    <a href="/">
+      <img src="/book-stand.png" alt="">     
+    </a>
   </section>
   <!-- <pre>{JSON.stringify(stories, null, 2)}</pre> -->
   <section class="stories">
-    {#each stories as data}
+    {#each stories as data, index}
       <article class="story">
         <section class="story__title-background">
           {@html prismic.asHTML(data.data.title)}
         </section>
-        <a class="story__link" href="/{data.uid}">Read story {data.uid} »</a>
+        <a class="story__link" href="/{data.uid}">Read story {index+1} »</a>
         <section class="story__image-background">
           <img class="story__image" src="{data.data.image.url}" alt="">
         </section>
@@ -25,17 +37,36 @@
   
 
 <style>
+  .hero {
+    flex: 100%;
+  }
+
   .create-story {
     margin-top: 2em;
-    max-width: 60em;
+    flex: 1;
+    align-items: center;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
   }
 
   .create-story__clock {
-    width: 18em;
-    height: 18em;
+    margin-bottom: 1em;
+    width: 16em;
+    height: 16em;
     display: none;
+  }
+
+  .create-story__heading {
+    margin-bottom: .5em;
+    font-size: 2rem;
+    text-align: center;
+    text-decoration: none;
+    color: var(--white);
+    max-width: 10em;
+  }
+
+  .create-story__icon {
+    margin-bottom: 2em;
   }
 
   .stories {
@@ -47,6 +78,7 @@
     justify-content: flex-start;
     gap: 3em;
     overflow-x: scroll;
+    flex: 3;
   }
 
   .story {
@@ -121,6 +153,15 @@
 
     .create-story__clock {
       display: block;
+    }
+
+    .story {
+      margin-right: 2em;
+    }
+
+    .stories {
+      margin-right: 0;
+      margin-left: 0;
     }
   }
 </style>
